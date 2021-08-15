e0442b60fbaf48559dc2acf5d7d88a0e54540ae5
oliver abad <oliver@nexusgaming.tv>
2021-08-16 02:57:44 +0800

    update constant

--- app/src/main/java/com/oli/weatherforecast/util/Constants.kt
+++ app/src/main/java/com/oli/weatherforecast/util/Constants.kt
@@ -2,7 +2,7 @@ package com.oli.weatherforecast.util
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "0594b4d5e3598ca29c713bfb925688cc"
+    const val API_KEY = "YOUR API KEY"
     const val DEGREE_SYMBOL = "°"
     const val ICON_URL = "https://openweathermap.org/img/wn/"
 }
